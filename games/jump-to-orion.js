c775aa3ce4b6e0646dfa3949dbb43bd3fab62563
JasonDunken <jasondunken@yahoo.com>
2021-01-23 23:55:33 -0500

    first pass at jump to orion port

--- games/jump-to-orion.js
+++ games/jump-to-orion.js
@@ -1,22 +1,125 @@
 const WIDTH = 600;
 const HEIGHT = 400;
 
+let canvas;
+
+let img_background1;
+let img_background2;
+let img_foreground1;
+let scenery = [];
+
+let img_player;
+
 function setup() {
-  frameRate(15);
-  let canvas = createCanvas(WIDTH, HEIGHT);
+  frameRate(60);
+  canvas = createCanvas(WIDTH, HEIGHT);
   canvas.parent("game");
+
+  img_player = new Image();
+  img_player.src = "./jump-to-orion/img/sprite1.png";
+
+  img_background1 = new Image();
+  img_background1.src = "./jump-to-orion/img/space.png";
+  img_background1.xScroll = 0;
+  img_background1.xScrollSpeed = 1;
+  scenery.push(img_background1);
+  img_background2 = new Image();
+  img_background2.src = "./jump-to-orion/img/planets.png";
+  img_background2.xScroll = 0;
+  img_background2.xScrollSpeed = 2;
+  scenery.push(img_background2);
+  img_foreground1 = new Image();
+  img_foreground1.src = "./jump-to-orion/img/debris.png";
+  img_foreground1.xScroll = 0;
+  img_foreground1.xScrollSpeed = 5;
+  scenery.push(img_foreground1);
+}
+
+function update() {
+  for (let layer of scenery) {
+    layer.xScroll += layer.xScrollSpeed;
+    if (layer.xScroll >= layer.width) {
+      layer.xScroll = 0;
+    }
+  }
 }
 
 function draw() {
-  loadPixels();
-  for (let i = 0; i < WIDTH * HEIGHT * 4; i += 4) {
-    let flip = Math.random() < 0.5 ? 0 : 255;
-    //     r               g               b
-    pixels[i] = pixels[i + 1] = pixels[i + 2] = flip;
-    //         alpha
-    pixels[i + 3] = 255;
+  update();
+  background("black");
+  drawingContext.drawImage(
+    scenery[0],
+    scenery[0].xScroll,
+    0,
+    WIDTH,
+    HEIGHT,
+    0,
+    0,
+    WIDTH,
+    HEIGHT
+  );
+  if (scenery[0].xScroll > scenery[0].width - WIDTH) {
+    drawingContext.drawImage(
+      scenery[0],
+      scenery[0].xScroll - scenery[0].width,
+      0,
+      WIDTH,
+      HEIGHT,
+      0,
+      0,
+      WIDTH,
+      HEIGHT
+    );
   }
-  updatePixels();
+  drawingContext.drawImage(
+    scenery[1],
+    scenery[1].xScroll,
+    0,
+    WIDTH,
+    HEIGHT,
+    0,
+    0,
+    WIDTH,
+    HEIGHT
+  );
+  if (scenery[1].xScroll > scenery[1].width - WIDTH) {
+    drawingContext.drawImage(
+      scenery[1],
+      scenery[1].xScroll - scenery[1].width,
+      0,
+      WIDTH,
+      HEIGHT,
+      0,
+      0,
+      WIDTH,
+      HEIGHT
+    );
+  }
+  canvas.drawingContext.drawImage(img_player, 100, HEIGHT / 2);
+  drawingContext.drawImage(
+    scenery[2],
+    scenery[2].xScroll,
+    0,
+    WIDTH,
+    HEIGHT,
+    0,
+    0,
+    WIDTH,
+    HEIGHT
+  );
+  if (scenery[2].xScroll > scenery[2].width - WIDTH) {
+    drawingContext.drawImage(
+      scenery[2],
+      scenery[2].xScroll - scenery[2].width,
+      0,
+      WIDTH,
+      HEIGHT,
+      0,
+      0,
+      WIDTH,
+      HEIGHT
+    );
+  }
 }
 
 function setColor(newColor) {
