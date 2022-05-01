8659b97d8ddd15299d8f8487dea67b6ce9e184ac
jasondunken <jasondunken@yahoo.com>
2022-05-01 15:55:55 -0400

    implement varied alien movement on y axis

--- games/planet-invaders/alien.js
+++ games/planet-invaders/alien.js
@@ -1,23 +1,31 @@
 class Alien {
     delta = 0;
+    yRange = 5;
     constructor(size, position, speed, sprite) {
         this.size = size;
         this.pos = position;
         this.speed = speed;
         this.dead = false;
         this.sprite = sprite;
+        this.delta = Math.random() * 2 * PI;
+        this.yRange = Math.random() * this.yRange + this.yRange;
     }
 
     update() {
-        this.pos.y = this.pos.y + Math.sin(this.delta);
-        this.delta += 0.025;
-        if (this.delta >= 360) this.delta = 0;
+        this.delta += 0.25;
+        if (this.delta >= 2 * PI) this.delta = 0;
         this.pos.x = this.pos.x + this.speed;
     }
 
     render() {
         if (this.sprite) {
-            image(this.sprite, this.pos.x - this.size, this.pos.y - this.size, this.size * 2, this.size * 2);
+            image(
+                this.sprite,
+                this.pos.x - this.size,
+                this.pos.y - this.size + this.yRange * Math.sin(this.delta),
+                this.size * 2,
+                this.size * 2
+            );
         }
     }
 }
