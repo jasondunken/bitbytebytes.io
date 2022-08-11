d694a967fde91d49b7fa1ffc428e3a282e7c7bdb
jasondunken <jasondunken@yahoo.com>
2022-08-10 20:21:14 -0400

    formatting

--- games/games.js
+++ games/games.js
@@ -1,40 +1,40 @@
 const GAMES = [
-  {
-    title: "Air Defense",
-    description: "Defend your battery against aerial assult!",
-    icon: "../img/defense.png",
-    file: "air-defense.html",
-  },
-  {
-    title: "Mine Squad Plus",
-    description: "Hunt for hidden mines!",
-    icon: "../img/mine.png",
-    file: "minesquadplus.html",
-  },
-  {
-    title: "Planet Invaders",
-    description: "The invasion has begun, save the planet!",
-    icon: "../img/invaders.png",
-    file: "planet-invaders.html",
-  },
-  {
-    title: "Slider",
-    description: "Slide your way to insanity!",
-    icon: "../img/slider.png",
-    file: "slider.html",
-  },
-  {
-    title: "Jump to Orion",
-    description: "Taking it straight to the aliens!",
-    icon: "../img/orion.png",
-    file: "jump-to-orion.html",
-  },
+    {
+        title: "Air Defense",
+        description: "Defend your battery against aerial assult!",
+        icon: "../img/defense.png",
+        file: "air-defense.html",
+    },
+    {
+        title: "Mine Squad Plus",
+        description: "Hunt for hidden mines!",
+        icon: "../img/mine.png",
+        file: "minesquadplus.html",
+    },
+    {
+        title: "Planet Invaders",
+        description: "The invasion has begun, save the planet!",
+        icon: "../img/invaders.png",
+        file: "planet-invaders.html",
+    },
+    {
+        title: "Slider",
+        description: "Slide your way to insanity!",
+        icon: "../img/slider.png",
+        file: "slider.html",
+    },
+    {
+        title: "Jump to Orion",
+        description: "Taking it straight to the aliens!",
+        icon: "../img/orion.png",
+        file: "jump-to-orion.html",
+    },
 ];
 
 const body = document.querySelector("body");
 for (let game of GAMES) {
-  let menuDiv = document.createElement("div");
-  menuDiv.innerHTML = `<div class="container">
+    let menuDiv = document.createElement("div");
+    menuDiv.innerHTML = `<div class="container">
         <div class="card">
             <div class="icon">
                 <img src="${game.icon}" alt="${game.title}" />
@@ -49,35 +49,35 @@ for (let game of GAMES) {
         </div>
     </div>`;
 
-  const card = menuDiv.querySelector(".card");
-  const container = menuDiv.querySelector(".container");
-  const icon = menuDiv.querySelector(".icon img");
-  const title = menuDiv.querySelector(".title");
-  const description = menuDiv.querySelector(".info h3");
-  const play = menuDiv.querySelector(".play");
+    const card = menuDiv.querySelector(".card");
+    const container = menuDiv.querySelector(".container");
+    const icon = menuDiv.querySelector(".icon img");
+    const title = menuDiv.querySelector(".title");
+    const description = menuDiv.querySelector(".info h3");
+    const play = menuDiv.querySelector(".play");
 
-  container.addEventListener("mousemove", (e) => {
-    let bb = card.getBoundingClientRect();
-    let yRotation = (bb.x + bb.width / 2 - e.pageX) / 30;
-    let xRotation = (window.pageYOffset + bb.height / 2 - e.pageY) / 30;
-    if (xRotation > 10) xRotation = 10;
-    if (xRotation < -10) xRotation = -10;
-    card.style.transform = `rotateY(${-yRotation}deg) rotateX(${-xRotation}deg)`;
-  });
-  container.addEventListener("mouseenter", () => {
-    card.style.transition = "none";
-    icon.style.transform = "translateZ(150px)";
-    title.style.transform = "translateZ(125px)";
-    description.style.transform = "translateZ(125px)";
-    play.style.transform = "translateZ(150px)";
-  });
-  container.addEventListener("mouseleave", (e) => {
-    card.style.transition = "all 0.7s ease";
-    card.style.transform = `rotateY(0deg) rotateX(0deg)`;
-    icon.style.transform = "translateZ(0px)";
-    title.style.transform = "translateZ(0px)";
-    description.style.transform = "translateZ(0px)";
-    play.style.transform = "translateZ(0px)";
-  });
-  body.appendChild(menuDiv);
+    container.addEventListener("mousemove", (e) => {
+        let bb = card.getBoundingClientRect();
+        let yRotation = (bb.x + bb.width / 2 - e.pageX) / 30;
+        let xRotation = (window.pageYOffset + bb.height / 2 - e.pageY) / 30;
+        if (xRotation > 10) xRotation = 10;
+        if (xRotation < -10) xRotation = -10;
+        card.style.transform = `rotateY(${-yRotation}deg) rotateX(${-xRotation}deg)`;
+    });
+    container.addEventListener("mouseenter", () => {
+        card.style.transition = "none";
+        icon.style.transform = "translateZ(150px)";
+        title.style.transform = "translateZ(125px)";
+        description.style.transform = "translateZ(125px)";
+        play.style.transform = "translateZ(150px)";
+    });
+    container.addEventListener("mouseleave", (e) => {
+        card.style.transition = "all 0.7s ease";
+        card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+        icon.style.transform = "translateZ(0px)";
+        title.style.transform = "translateZ(0px)";
+        description.style.transform = "translateZ(0px)";
+        play.style.transform = "translateZ(0px)";
+    });
+    body.appendChild(menuDiv);
 }
