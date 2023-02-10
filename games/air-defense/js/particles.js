a8c06d0f03da766d8cb06b56e7069d16ec2c8e1a
jasondunken <jasondunken@yahoo.com>
2023-02-09 21:45:06 -0500

    consolidate particle effects

--- games/air-defense/js/particles.js
+++ games/air-defense/js/particles.js
@@ -1,3 +1,70 @@
+class Explosion extends GameObject {
+    duration = 30;
+    MAX_RADIUS = 25;
+    constructor(position) {
+        super("explosion", position);
+        const sound = new Audio();
+        sound.src = "./air-defense/snd/explosion_1.wav";
+        sound.play();
+    }
+
+    update() {
+        this.duration--;
+        if (this.duration <= 0) this.dead = true;
+    }
+
+    render() {
+        const radius = Math.random() * this.MAX_RADIUS;
+        const color = `rgba(${Math.floor(Math.random() * 128) + 128}, 0, 0, ${Math.floor(Math.random() * 256)})`;
+        setColor(color);
+        ellipse(this.position.x, this.position.y, radius, radius);
+    }
+}
+
+class Splatter extends GameObject {
+    MAX_DURATION = 100;
+    MAX_PARTICLES = 5;
+    MAX_RADIUS = 3;
+    SPLAT_GRAVITY = 3;
+    particles = new Set();
+    constructor(position, direction) {
+        super("splatter", position);
+        for (let i = 0; i < this.MAX_PARTICLES; i++) {
+            this.particles.add({
+                id: i,
+                position: {
+                    x: position.x + Math.random() * 10 - 5,
+                    y: position.y + Math.random() * 10 - 5,
+                },
+                direction: {
+                    x: (direction.x + Math.random() * 5 - 2.5) / 10,
+                    y: (direction.y + Math.random() * 5 - 2.5) / 10,
+                },
+                radius: Math.random() * this.MAX_RADIUS + 1,
+                life: Math.random() * (this.MAX_DURATION / 2) + this.MAX_DURATION / 2,
+            });
+        }
+    }
+
+    update() {
+        this.particles.forEach((particle) => {
+            particle.position.x = particle.position.x + particle.direction.x;
+            particle.position.y = particle.position.y + particle.direction.y + this.SPLAT_GRAVITY;
+            particle.life--;
+            if (particle.life <= 0) this.particles.delete(particle);
+        });
+        if (this.particles.size === 0) this.dead = true;
+    }
+
+    render() {
+        this.particles.forEach((particle) => {
+            const color = `rgba(${Math.floor(Math.random() * 128) + 128}, 0, 0, ${Math.floor(Math.random() * 256)})`;
+            setColor(color);
+            ellipse(particle.position.x, particle.position.y, particle.radius, particle.radius);
+        });
+    }
+}
+
 class ParticleEmitter {
     constructor(pos, dir, amount) {
         this.particles = [];
@@ -38,6 +105,7 @@ class Particle {
         if (this.life <= 0) {
             this.dead = true;
         }
+        console.log("bulletSpeed: ", bulletSpeed);
         this.pos.x = this.pos.x + this.dir.x / (bulletSpeed * 2);
         this.pos.y = this.pos.y + this.dir.y / (bulletSpeed * 2);
     }
