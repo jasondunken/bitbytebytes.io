e29792710159b6abd40f9517744851cdffd84b45
jasondunken <jasondunken@yahoo.com>
2023-05-15 13:07:28 -0400

    mine-squad => ui tweeks

--- games/mine-squad/modules/ui.js
+++ games/mine-squad/modules/ui.js
@@ -4,19 +4,13 @@ import { GAME_STATE } from "./game-state.js";
 import { getElapsedTimeString } from "./utils.js";
 
 class UI {
+    P5_TEXT_ISNT_REALLY_CENTERED = 5;
     UI_HEIGHT = 50;
     UI_WIDTH = 952;
 
     UI_MARGIN = 4;
 
     UI_BOX_Y = 18;
-
-    UI_BOX_1_X = 4;
-    UI_BOX_2_X = 72;
-    UI_BOX_3_X = 412;
-    UI_BOX_4_X = 652;
-    UI_BOX_5_X = 752;
-    UI_BOX_6_X = 852;
 
     UI_BOX_HEIGHT = 28;
 
@@ -29,8 +23,17 @@ class UI {
     SCORE_BOX_WIDTH = 128;
     SQUADS_BOX_WIDTH = 128;
     TIME_BOX_WIDTH = 96;
-    TILES_BOX_WIDTH = 96;
+    HIDDEN_BOX_WIDTH = 96;
     FLAGS_BOX_WIDTH = 96;
+
+    LEVEL_BOX_X = 4;
+    SCORE_BOX_X = 72;
+    SQUADS_BOX_X = 412;
+    TIME_BOX_X = 652;
+    HIDDEN_BOX_X = 752;
+    FLAGS_BOX_X = 852;
+
+    SQUADS_SPACING = 36;
 
     constructor(mineSquad, position) {
         this.mineSquad = mineSquad;
@@ -70,10 +73,10 @@ class UI {
         fill("white");
         textSize(14);
         textAlign(LEFT, BOTTOM);
-        text("LEVEL", this.position.x + this.UI_BOX_1_X, this.position.y + this.UI_BOX_Y);
+        text("LEVEL", this.position.x + this.LEVEL_BOX_X, this.position.y + this.UI_BOX_Y);
         fill(color(44, 44, 44));
         rect(
-            this.position.x + this.UI_BOX_1_X,
+            this.position.x + this.LEVEL_BOX_X,
             this.position.y + this.UI_BOX_Y,
             this.LEVEL_BOX_WIDTH,
             this.UI_BOX_HEIGHT
@@ -83,7 +86,7 @@ class UI {
         textAlign(CENTER, CENTER);
         text(
             "" + this.gameData.level,
-            this.UI_BOX_1_X + this.LEVEL_BOX_WIDTH / 2,
+            this.LEVEL_BOX_X + this.LEVEL_BOX_WIDTH / 2 + this.P5_TEXT_ISNT_REALLY_CENTERED,
             this.position.y + this.UI_BOX_Y + this.UI_VALUE_CENTER_Y
         );
     }
@@ -93,10 +96,10 @@ class UI {
         fill("white");
         textSize(14);
         textAlign(LEFT, BOTTOM);
-        text("SCORE", this.position.x + this.UI_BOX_2_X, this.position.y + this.UI_BOX_Y);
+        text("SCORE", this.position.x + this.SCORE_BOX_X, this.position.y + this.UI_BOX_Y);
         fill(color(44, 44, 44));
         rect(
-            this.position.x + this.UI_BOX_2_X,
+            this.position.x + this.SCORE_BOX_X,
             this.position.y + this.UI_BOX_Y,
             this.SCORE_BOX_WIDTH,
             this.UI_BOX_HEIGHT
@@ -106,7 +109,7 @@ class UI {
         textAlign(CENTER, CENTER);
         text(
             "" + this.gameData.score,
-            this.UI_BOX_2_X + this.SCORE_BOX_WIDTH / 2,
+            this.SCORE_BOX_X + this.SCORE_BOX_WIDTH / 2 + this.P5_TEXT_ISNT_REALLY_CENTERED,
             this.position.y + this.UI_BOX_Y + this.UI_VALUE_CENTER_Y
         );
     }
@@ -116,10 +119,10 @@ class UI {
         fill("white");
         textSize(14);
         textAlign(LEFT, BOTTOM);
-        text("SQUADS", this.position.x + this.UI_BOX_3_X, this.position.y + this.UI_BOX_Y);
+        text("SQUADS", this.position.x + this.SQUADS_BOX_X, this.position.y + this.UI_BOX_Y);
         fill(color(44, 44, 44));
         rect(
-            this.position.x + this.UI_BOX_3_X,
+            this.position.x + this.SQUADS_BOX_X,
             this.position.y + this.UI_BOX_Y,
             this.SQUADS_BOX_WIDTH,
             this.UI_BOX_HEIGHT
@@ -136,9 +139,9 @@ class UI {
                     fill("Green");
                 }
             }
-            const xOff = 32 * (i - 1);
+            const xOff = this.SQUADS_SPACING * (i - 1);
             ellipse(
-                this.position.x + this.UI_BOX_3_X + this.SCORE_BOX_WIDTH / 2 + xOff,
+                this.position.x + this.SQUADS_BOX_X + this.SCORE_BOX_WIDTH / 2 + xOff,
                 this.position.y + this.UI_BOX_Y + this.UI_VALUE_CENTER_Y,
                 12,
                 12
@@ -151,10 +154,10 @@ class UI {
         fill("white");
         textSize(14);
         textAlign(LEFT, BOTTOM);
-        text("TIME", this.position.x + this.UI_BOX_4_X, this.position.y + this.UI_BOX_Y);
+        text("TIME", this.position.x + this.TIME_BOX_X, this.position.y + this.UI_BOX_Y);
         fill(color(44, 44, 44));
         rect(
-            this.position.x + this.UI_BOX_4_X,
+            this.position.x + this.TIME_BOX_X,
             this.position.y + this.UI_BOX_Y,
             this.TIME_BOX_WIDTH,
             this.UI_BOX_HEIGHT
@@ -164,7 +167,7 @@ class UI {
         textAlign(CENTER, CENTER);
         text(
             "" + getElapsedTimeString(this.gameData.time),
-            this.UI_BOX_4_X + this.TIME_BOX_WIDTH / 2,
+            this.TIME_BOX_X + this.TIME_BOX_WIDTH / 2 + this.P5_TEXT_ISNT_REALLY_CENTERED,
             this.position.y + this.UI_BOX_Y + this.UI_VALUE_CENTER_Y
         );
     }
@@ -174,12 +177,12 @@ class UI {
         fill("white");
         textSize(14);
         textAlign(LEFT, BOTTOM);
-        text("HIDDEN", this.position.x + this.UI_BOX_5_X, this.position.y + this.UI_BOX_Y);
+        text("HIDDEN", this.position.x + this.HIDDEN_BOX_X, this.position.y + this.UI_BOX_Y);
         fill(color(44, 44, 44));
         rect(
-            this.position.x + this.UI_BOX_5_X,
+            this.position.x + this.HIDDEN_BOX_X,
             this.position.y + this.UI_BOX_Y,
-            this.TILES_BOX_WIDTH,
+            this.HIDDEN_BOX_WIDTH,
             this.UI_BOX_HEIGHT
         );
         textSize(18);
@@ -188,14 +191,14 @@ class UI {
             fill("red");
             text(
                 "" + this.gameData.hidden,
-                this.UI_BOX_5_X + this.TILES_BOX_WIDTH / 2,
+                this.HIDDEN_BOX_X + this.HIDDEN_BOX_WIDTH / 2 + this.P5_TEXT_ISNT_REALLY_CENTERED,
                 this.position.y + this.UI_BOX_Y + this.UI_VALUE_CENTER_Y
             );
         } else {
             fill("black");
             text(
                 "X",
-                this.UI_BOX_5_X + this.TILES_BOX_WIDTH / 2,
+                this.HIDDEN_BOX_X + this.HIDDEN_BOX_WIDTH / 2 + this.P5_TEXT_ISNT_REALLY_CENTERED,
                 this.position.y + this.UI_BOX_Y + this.UI_VALUE_CENTER_Y
             );
         }
@@ -206,12 +209,12 @@ class UI {
         fill("white");
         textSize(14);
         textAlign(LEFT, BOTTOM);
-        text("FLAGS", this.position.x + this.UI_BOX_6_X, this.position.y + this.UI_BOX_Y);
+        text("FLAGS", this.position.x + this.FLAGS_BOX_X, this.position.y + this.UI_BOX_Y);
         fill(color(44, 44, 44));
         rect(
-            this.position.x + this.UI_BOX_6_X,
+            this.position.x + this.FLAGS_BOX_X,
             this.position.y + this.UI_BOX_Y,
-            this.TILES_BOX_WIDTH,
+            this.HIDDEN_BOX_WIDTH,
             this.UI_BOX_HEIGHT
         );
         textSize(18);
@@ -220,14 +223,14 @@ class UI {
             fill("yellow");
             text(
                 "" + this.gameData.flags,
-                this.UI_BOX_6_X + this.FLAGS_BOX_WIDTH / 2,
+                this.FLAGS_BOX_X + this.FLAGS_BOX_WIDTH / 2 + this.P5_TEXT_ISNT_REALLY_CENTERED,
                 this.position.y + this.UI_BOX_Y + this.UI_VALUE_CENTER_Y
             );
         } else {
             fill("black");
             text(
                 "X",
-                this.UI_BOX_6_X + this.FLAGS_BOX_WIDTH / 2,
+                this.FLAGS_BOX_X + this.FLAGS_BOX_WIDTH / 2 + this.P5_TEXT_ISNT_REALLY_CENTERED,
                 this.position.y + this.UI_BOX_Y + this.UI_VALUE_CENTER_Y
             );
         }
