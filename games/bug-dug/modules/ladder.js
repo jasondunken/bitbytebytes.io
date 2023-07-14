8bf0b2a5b67d8a8ad9b5f38065672323ac96c605
jasondunken <jasondunken@yahoo.com>
2023-07-13 21:28:38 -0400

    bug-dug => add ladder class

---
+++ games/bug-dug/modules/ladder.js
@@ -0,0 +1,21 @@
+import { rejects } from "assert";
+import { GameObject } from "../../modules/gameObject.js";
+
+class Ladder extends GameObject {
+    constructor(position, sprite) {
+        super("ladder", position);
+        this.sprite = sprite;
+    }
+
+    render() {
+        if (this.sprite) {
+            Image(this.sprite, this.position.x, this.position.y, 32, 32);
+        } else {
+            noStroke();
+            fill("magenta");
+            rect(this.position.x, this.position.y, 32, 32);
+        }
+    }
+}
+
+export { Ladder };
