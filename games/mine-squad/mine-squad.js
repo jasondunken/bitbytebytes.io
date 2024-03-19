2703143f1de406f92d5bc9e9c229c2563c05613e
jasondunken <jasondunken@yahoo.com>
2024-03-19 08:27:17 -0400

    mine-squad => change mouse event mouse down -> mouse up

--- games/mine-squad/mine-squad.js
+++ games/mine-squad/mine-squad.js
@@ -12,7 +12,8 @@ window.preload = preload;
 window.setup = setup;
 window.draw = draw;
 window.keyPressed = keyPressed;
-window.mousePressed = mousePressed;
+//window.mousePressed = mousePressed;
+window.addEventListener("mouseup", mousePressed);
 
 const GAME_WIDTH = 960;
 const GAME_HEIGHT = 540;
